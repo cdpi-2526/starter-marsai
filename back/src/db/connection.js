20f3ecaa89c47333e161bbebd2d0016bb661ba19
Sambeau <sambeauprak@gmail.com>
2026-01-27 16:40:55 +0100

    refactor: revert database connection to hardcoded values for local development

--- back/src/db/connection.js
+++ back/src/db/connection.js
@@ -1,15 +1,10 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-  },
-);
+const sequelize = new Sequelize("marsai", "root", "rootroot", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+});
 
 sequelize.sync().then(() => {
   console.log("La base de données est synchronisée.");
