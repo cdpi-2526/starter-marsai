2c6fddc7cf03f052d8647234b03412842d7428eb
Sambeau <sambeauprak@gmail.com>
2026-01-27 10:07:23 +0100

    refactor: uncomment AuthMiddleware for ADMIN role and comment out PRODUCER role in User routes feat: add token removal in handleLogout function in Navbar component

--- back/src/routes/User.route.js
+++ back/src/routes/User.route.js
@@ -6,7 +6,7 @@ const userRouter = express.Router();
 
 // Admin
 
-// userRouter.use((req, res, next) => AuthMiddleware(req, res, next, ["ADMIN"]));
+userRouter.use((req, res, next) => AuthMiddleware(req, res, next, ["ADMIN"]));
 
 userRouter.get("/", UserController.getUsers); // Liste de tous les utilisateurs
 userRouter.get("/:id", UserController.getUserById); // Récupérer un utilisateur par ID
@@ -14,7 +14,7 @@ userRouter.post("/", UserController.createUser); // Créer un nouvel utilisateur
 userRouter.delete("/:id", UserController.deleteUser); // Supprimer un utilisateur par ID
 userRouter.put(
   "/:id",
-  (req, res, next) => AuthMiddleware(req, res, next, ["PRODUCER"]),
+  // (req, res, next) => AuthMiddleware(req, res, next, ["PRODUCER"]),
   UserController.updateUser,
 ); // Modifier un utilisateur par ID
 
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ export default function Navbar() {
   function handleLogout() {
     localStorage.removeItem("username");
     localStorage.removeItem("role");
+    localStorage.removeItem("token");
     window.location.reload();
   }
 
