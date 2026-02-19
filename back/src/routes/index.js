aa7a4b5feb3b96e05c425a48600088f38621ac73
Sambeau <sambeauprak@gmail.com>
2026-02-19 14:38:20 +0100

    refactor: remove unused route imports and middleware from index.js

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,16 +1,5 @@
 import express from "express";
-import userRouter from "./User.route.js";
-import videoRouter from "./Video.route.js";
-import authRouter from "./Auth.route.js";
-import youtubeRouter from "./Youtube.route.js";
-import awardRouter from "./Award.route.js";
 
 const router = express.Router();
-
-router.use("/auth", authRouter);
-router.use("/users", userRouter);
-router.use("/videos", videoRouter);
-router.use("/youtube", youtubeRouter);
-router.use("/awards", awardRouter);
 
 export default router;
